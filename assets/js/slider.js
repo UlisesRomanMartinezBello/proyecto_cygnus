24c3eb56145b4ead9c41ded1ac912b793e7afa88
Eduardo Larios Juseppe <yp.ja44@gmail.com>
2022-05-25 11:08:08 -0500

    agregando cosas al index

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -10,8 +10,55 @@ window.addEventListener('load', function(){
 		},
 		responsive: [
 			{
+				// screens greater than >= 775px
+				breakpoint: 200,
+				settings: {
+				  // Set to `auto` and provide item width to adjust to viewport
+				  slidesToShow: 2,
+				  slidesToScroll: 2
+				}
+			  },{
 			  // screens greater than >= 775px
-			  breakpoint: 775,
+			  breakpoint: 475,
+			  settings: {
+				// Set to `auto` and provide item width to adjust to viewport
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  // screens greater than >= 1024px
+			  breakpoint: 1024,
+			  settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4
+			  }
+			}
+		]
+	});
+});
+
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista1'), {
+		// slidesToShow: 4,
+		// slidesToScroll: 4,
+		draggable: true,
+		dots: '.carousel__indicadores1',
+		arrows: {
+			prev: '.carousel__anterior1',
+			next: '.carousel__siguiente1'
+		},
+		responsive: [
+			{
+				// screens greater than >= 775px
+				breakpoint: 200,
+				settings: {
+				  // Set to `auto` and provide item width to adjust to viewport
+				  slidesToShow: 2,
+				  slidesToScroll: 2
+				}
+			  },{
+			  // screens greater than >= 775px
+			  breakpoint: 475,
 			  settings: {
 				// Set to `auto` and provide item width to adjust to viewport
 				slidesToShow: 2,
